42ce24532fcbf9a8b2eec3c03d54a40b3a921a98
U-yunliao-PC\yunliao <huyunliao@126.com>
2020-06-09 13:53:59 +0800

    update Algorithm Description

--- uniform.c
+++ uniform.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+/*
+ * Algorithm Description:
+ * the pdf of uniform distribution is:
+ * f(x) = 1/(b-a) if a <= x <= b else 0
+ * the mean if (a+b)/2 and the varience is (a-b)^2/12
+ *
+ * generating algorithm:
+ * x_i = (k*x_i-1 + c) mod M
+ * y_i = x_i / M
+ * z_i = a + (b-a)*y_i then z_i is between in (a,b)
+ */
+
+
 // generate the random double number between a and b
 double uniform(double a, double b, long int *seed){
 	double t;
