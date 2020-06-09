05c4f3b209161a770ae129b923ca037983ff6c19
U-yunliao-PC\yunliao <huyunliao@126.com>
2020-06-09 14:03:35 +0800

    add real random seed with time

--- uniform.c
+++ uniform.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /*
  * Algorithm Description:
  * the pdf of uniform distribution is:
@@ -26,7 +28,10 @@ int main(){
 	double a,b,x;
 	int i,j;
 	long int s;
-	a = 0.0;b = 1.0; s = 13579;
+	a = 0.0;b = 1.0;
+   	/* s = 13579;*/
+	srand((unsigned)time(NULL));
+	s = rand() % 10000 + 1;
 	for(i=0; i<10; i++){
 		for(j=0; j<5; j++){
 			x = uniform(a,b,&s);
