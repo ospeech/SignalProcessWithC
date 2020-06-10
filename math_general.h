6bbbb1065a50d30744df3c212a7bb76f6cc3fe61
U-yunliao-PC\yunliao <huyunliao@126.com>
2020-06-10 17:19:15 +0800

    rectify some typos

--- math_general.h
+++ math_general.h
@@ -1,7 +1,7 @@
 #ifndef MATH_GENERAL_H_
 #define MATH_GENERAL_H_
-/*
- * Algorithm Description:
+
+/* Algorithm Description:
  * the pdf of uniform distribution is:
  * f(x) = 1/(b-a) if a <= x <= b else 0
  * the mean if (a+b)/2 and the varience is (a-b)^2/12
@@ -11,7 +11,6 @@
  * y_i = x_i / M
  * z_i = a + (b-a)*y_i then z_i is between in (a,b)
  */
-
 
 // generate the random double number between a and b
 double uniform(double a, double b, long int *seed){
@@ -24,12 +23,13 @@ double uniform(double a, double b, long int *seed){
 }
 
 /* Algorithm Description:
-suppose r_1,r_2 ... is according to uniform distribution (0,1).
-E(r_i) = 1/2, D(r_i) = 1/12. Acoording to Central Limit Theorems，
-x = sqrt(12/n) * (Sigma(r_i) - n/2) closed to N(0,1) while n is so bigger.
-normally, n=12, and x = Sigma(r_i) - 6,
-y = mean + sigma * x and y closed to N(mean, sigma^2).
-*/
+ * suppose r_1,r_2 ... is according to uniform distribution (0,1).
+ * E(r_i) = 1/2, D(r_i) = 1/12. Acoording to Central Limit Theorems，
+ * x = sqrt(12/n) * (Sigma(r_i) - n/2) closed to N(0,1) while n is so bigger.
+ * normally, n=12, and x = Sigma(r_i) - 6,
+ * y = mean + sigma * x and y closed to N(mean, sigma^2).
+ */
+
 // generate the random double number regarding Gaussian distribution N(u,o^2)
 double gauss(double mean, double sigma, long int *seed){
 	int i; double x, y;
@@ -43,6 +43,5 @@ double gauss(double mean, double sigma, long int *seed){
 	
 	return y;
 }
-
 
 #endif
